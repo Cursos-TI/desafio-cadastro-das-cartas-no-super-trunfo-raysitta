4eb91b32c13aef37a130f87e1ce5f56d3fce4abb
raysitta <rayssaalves555almeida@gmail.com>
2025-05-20 17:49:41 +0000

    Adicionado comentario para facilitar a leitura do código-fonte como pedido.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,21 +9,21 @@ int main(){
     float altura2 = 783.79;
     int idade = 100;
 
-    printf("Desafio Cartas Super Trunfo!\n");
+    printf("Desafio Cartas Super Trunfo!\n"); // Comentário nome do Desafio.
 
-    printf("Estado: A \n");
+    printf("Estado: A \n"); // Comentrário letra da Carta.
 
-    printf("Código: %s\n", nome);
+    printf("Código: %s\n", nome); //Usado %s para imprimir a string nome/código.
 
-    printf("Nome da cidade: %s\n", cidade); 
+    printf("Nome da cidade: %s\n", cidade); //Usado %s para imprimir a string cidade.
 
-    printf("População: %f\n", quantidade);
+    printf("População: %f\n", quantidade); //Usado %f para imprimir a float quantidade.
 
-    printf("Área: %f\n", altura);
+    printf("Área: %f\n", altura); //Usado %f para imprimir a float altura.
 
-    printf("PIB: %f\n", altura2);
+    printf("PIB: %f\n", altura2); //Usado %f para imprimir a float altura2.
 
-    printf("Número de Pontos Túristicos: %d\n", idade);
+    printf("Número de Pontos Túristicos: %d\n", idade); //Usado %d para imprimir o int idade.
 
     return 0;
 
